1a1225bff38317b94b1fe41b3a81a3ff48228831
shiven143p <shiven143p@gmail.com>
2024-05-27 12:37:19 +0530

    Initial Upload of project files on 27 May 2024
    Initial Upload of project files on 27 May 2024

---
+++ ContactManagement.Services/IContactService.cs
@@ -0,0 +1,14 @@
+﻿using ContactManangement.Entities;
+using ContactManangement.Entities.Dto;
+
+namespace ContactManangement.Services
+{
+    public interface IContactService
+    {
+        public Task<InternalResponse> GetContactListAsync();
+        public Task<InternalResponse> GetContactByIDAsync(string contactId);
+        public Task<InternalResponse> AddContactAsync(ContactDto contactDto);
+        public Task<InternalResponse> UpdateContactAsync(ContactDto contactDto);
+        public Task<InternalResponse> DeleteContactAsync(string contactId);
+    }
+}
